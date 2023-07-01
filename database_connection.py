0ff8bc1f4735d0c87abd244bf36eeff7ecdf6d64
Ivana <imilutinovic119@gmail.com>
2023-07-01 13:52:03 +0200

    kNN implemented with diff metrics

--- database_connection.py
+++ database_connection.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import numpy as np
 
 def connect():
     client = MongoClient('mongodb://nastava.is.pmf.uns.ac.rs:27017/')
@@ -8,6 +9,15 @@ def connect():
 
     return collection
 
+def get_image_collection():
+    images = connect().find()
+    image_embeddings = []
+    for image in images:
+        embedding = image['embedding']
+        image_embeddings.append(embedding)
+    image_embeddings = np.array(image_embeddings)
+    return image_embeddings
+
 def insert_embedd(collection, image_name, embedding):
     embedding_doc = {
         "image_name": image_name,
@@ -16,3 +26,10 @@ def insert_embedd(collection, image_name, embedding):
 
     collection.insert_one(embedding_doc)
     print("Successfully saved!")
+
+def find_document_by_embedding(embedding):
+    query = {"embedding": embedding}
+    collection = connect()
+    document = collection.find_one(query)
+    #only one document found and return
+    return document
--- image_database.py
+++ image_database.py
@@ -3,10 +3,12 @@ from dataset_loader import image_preprocessor
 import database_connection as db
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics.pairwise import cosine_similarity
 import lucene_functions as luf
 
-#ovde treba zapravo da indeksiramo slike, samo ja nisam uspela PyLucene da uvedem u python :(
+#ukoliko tako zelimo, mozemo zapravo da indeksiramo slike, samo ja nisam uspela PyLucene da uvedem u python :(
 #mada mislim da ima preko dockera da se preuzme, pa da radi, stavila sam upustvo u txt fajl
+#takodje sam u lucene_functions.py stavila kod za indeksiranje i pretragu
 #TODO (@Ivana): finish everything below
 def index_images():
   clip_model=CLIP()
@@ -25,36 +27,53 @@ def index_images():
   #Ovde bi onda nakon sto smo sacuvali sve podatke u bazu, trebalo da ih indeksiramo 
   luf.images_indexing(collection.find())
 
-def get_k_most_similar_images(text_embedding, k):
+def get_k_most_similar_images(text_embedding, image_embeddings, k):
   #TODO: query database for most similar image using HNSW algorithm, use l2 distance for similarity
   #TODO: also have function to return top k most similar images, possibly also to sort whole database by similarity?
-  collection = db.connect() #make connection
   #here is basic knn
-  # get all embeddings from database
-  embeddings = []
-  for document in collection.find():
-    embedding = document['embedding']
-    embeddings.append(embedding)
-  embeddings = np.array(embeddings)
   knn = NearestNeighbors(n_neighbors=k, metric='cosine') #mozemo menjati metriku, Euclidian, Cosine, Manhattan..
-  knn.fit(embeddings)
-
+  knn.fit(image_embeddings)
   distances, indices = knn.kneighbors([text_embedding])
   """
   for distance, index in zip(distances[0], indices[0]):
     result = collection.find_one({'embedding': embeddings[index]})
     print(': {:.2f}, Document: {}'.format(distance, result))
     """
+  
+#maybe better version of implementing kNN
+def get_k_most_similar_images_sec(text_embedding, image_embeddings, k):
+    # Compute cosine similarity between text and every image in database
+    similarities = cosine_similarity(text_embedding[np.newaxis, :], image_embeddings)
+    # K nearest images:
+    nearest_indices = np.argsort(similarities[0])[::-1][:k] #find the closest embedding
+    image_doc = db.find_document_by_embedding(nearest_indices)
+    return image_doc #return image doc which continues the closest embedding
 
-def get_most_similar_image(text_embedding):
-  return get_k_most_similar_images(text_embedding,1)[0]
+def get_k_most_similar_images_L2(query_embedding, image_embeddings, k=5):
+    computed_distances = []
+    # Computed distances by Euclidian (L2) distance
+    for i, embedding in enumerate(image_embeddings):
+        distance = np.linalg.norm(query_embedding - embedding)
+        computed_distances.append((i, distance))
+    
+    # Sort image embeddings according to distance
+    computed_distances.sort(key=lambda x: x[1])
+    
+    # K neairest embeddings
+    nearest_indices = [index for index, _ in computed_distances[:k]] 
+    image_doc = db.find_document_by_embedding(nearest_indices)
+    return image_doc #return image doc which continues the closest embedding
+
+def get_most_similar_image(text_embedding, image_embeddings, k):
+  return get_k_most_similar_images(text_embedding, image_embeddings, k)[0]
 
 
 def find_most_similar_image(user_text_input):
   clip_model=CLIP()
   clip_model.load_model("models/CLIP")
   text_embedding=clip_model.getTextEmbedding(user_text_input)
-  result_image_name=get_most_similar_image(text_embedding)
-  #TODO: load everything from database properly ??
+  #TODO: load everything from database properly done 
+  image_collection = db.get_image_collection() #actually returns image embeddings from database
+  result_image_name=get_most_similar_image(text_embedding, image_collection, 1)
   result_image_contents=result_image_name
   return result_image_contents

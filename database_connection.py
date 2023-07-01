5ffbe7956d00ebce11fa0a2d98c10ad72b998ba7
Ivana <imilutinovic119@gmail.com>
2023-07-01 20:07:45 +0200

    prepared for tomorrow testing

--- database_connection.py
+++ database_connection.py
@@ -4,31 +4,34 @@ import numpy as np
 def connect():
     client = MongoClient('mongodb://nastava.is.pmf.uns.ac.rs:27017/')
     db = client['databases']
-
-    collection = db['imageEmbeddings']
-
+    collection = db['imageDocuments']
     return collection
 
 def get_image_collection():
     images = connect().find()
     image_embeddings = []
     for image in images:
-        embedding = image['embedding']
+        embedding = image['image_embedding']
         image_embeddings.append(embedding)
     image_embeddings = np.array(image_embeddings)
     return image_embeddings
 
-def insert_embedd(collection, image_name, embedding):
-    embedding_doc = {
+def insert_embedd(collection, image_id, image_name, real_image, 
+                  image_embedding, caption_to_use, text_embedding):
+    image_doc = {
+        "image_id": image_id,
         "image_name": image_name,
-        "embedding": embedding,
-    }
+        "real_image": real_image,
+        "image_embedding": image_embedding,
+        "caption_to_use": caption_to_use,
+        "text_embedding":text_embedding,
 
-    collection.insert_one(embedding_doc)
-    print("Successfully saved!")
+    }
+    collection.insert_one(image_doc)
+    #print("Successfully saved!")
 
 def find_document_by_embedding(embedding):
-    query = {"embedding": embedding}
+    query = {"image_embedding": embedding}
     collection = connect()
     document = collection.find_one(query)
     #only one document found and return
--- image_database.py
+++ image_database.py
@@ -1,35 +1,29 @@
-from clip_model import CLIP
 from dataset_loader import image_preprocessor
 import database_connection as db
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics.pairwise import cosine_similarity
-import lucene_functions as luf
+from dataset_loader import load_coco_dataset
+from clip_model import CLIP
 
-#ukoliko tako zelimo, mozemo zapravo da indeksiramo slike, samo ja nisam uspela PyLucene da uvedem u python :(
-#mada mislim da ima preko dockera da se preuzme, pa da radi, stavila sam upustvo u txt fajl
-#takodje sam u lucene_functions.py stavila kod za indeksiranje i pretragu
 #TODO (@Ivana): finish everything below
 def index_images():
-  clip_model=CLIP()
-  clip_model.load_model("models/CLIP")  
-  #TODO: load images from some dataset (TBD which one) ??? Mihailo da vidi sta ce sa ovim
-  images=[("image_name_1","contents1"),("image_name_2","contents2")]
-  image_embeddings_csv=open("image_embeddings.csv","w")
-  collection = db.connect() #make connection to database
-  for (image_name,image_conents) in images:
-    image_contents=image_preprocessor(image_contents)
-    image_embedding=clip_model.getImageEmbedding(image_conents)
-    #TODO: (DONE) save contents to database properly, use a database (probably mongodb or something similar)
-    db.insert_embedd(collection, image_name, image_embedding) # saved to MongoDB database (fakultetska baza)
-    #image_embeddings_csv.write(f"{image_name},{image_embedding}\n")
-
-  #Ovde bi onda nakon sto smo sacuvali sve podatke u bazu, trebalo da ih indeksiramo 
-  luf.images_indexing(collection.find())
+  batch_size=1
+  model=CLIP(batch_size=batch_size)
+  model.load_model()
+  for split in ["test","train"]:
+    data=load_coco_dataset(split,batch_size=batch_size,preprocessing_type="indexing")
+    collection = db.connect() #make connection to database
+    for (real_image,train_image,captions,image_filename,image_id) in data:
+      caption_to_use=model.get_random_captions(captions)
+      text_embedding=model.get_text_embedding(caption_to_use)
+      image_embedding=model.get_image_embedding(train_image)
+      #print("saving data:",real_image,image_embedding,caption_to_use,text_embedding,image_filename,image_id)
+      # saved to MongoDB database (fakultetska baza)
+      db.insert_embedd(collection, image_id, image_filename, real_image, image_embedding, caption_to_use, text_embedding)
+      #break
 
 def get_k_most_similar_images(text_embedding, image_embeddings, k):
-  #TODO: query database for most similar image using HNSW algorithm, use l2 distance for similarity
-  #TODO: also have function to return top k most similar images, possibly also to sort whole database by similarity?
   #here is basic knn
   knn = NearestNeighbors(n_neighbors=k, metric='cosine') #mozemo menjati metriku, Euclidian, Cosine, Manhattan..
   knn.fit(image_embeddings)
@@ -47,8 +41,10 @@ def get_k_most_similar_images_sec(text_embedding, image_embeddings, k):
     # K nearest images:
     nearest_indices = np.argsort(similarities[0])[::-1][:k] #find the closest embedding
     image_doc = db.find_document_by_embedding(nearest_indices)
-    return image_doc #return image doc which continues the closest embedding
+    return image_doc #return image doc which contains the closest embedding
 
+  #TODO: query database for most similar image, use l2 distance for similarity
+  #TODO: also have function to return top k most similar images
 def get_k_most_similar_images_L2(query_embedding, image_embeddings, k=5):
     computed_distances = []
     # Computed distances by Euclidian (L2) distance
@@ -62,18 +58,17 @@ def get_k_most_similar_images_L2(query_embedding, image_embeddings, k=5):
     # K neairest embeddings
     nearest_indices = [index for index, _ in computed_distances[:k]] 
     image_doc = db.find_document_by_embedding(nearest_indices)
-    return image_doc #return image doc which continues the closest embedding
+    return image_doc #return image doc which contains the the closest embedding
 
 def get_most_similar_image(text_embedding, image_embeddings, k):
-  return get_k_most_similar_images(text_embedding, image_embeddings, k)[0]
-
+  return get_k_most_similar_images(text_embedding, image_embeddings, k)["image_name"]
 
 def find_most_similar_image(user_text_input):
   clip_model=CLIP()
   clip_model.load_model("models/CLIP")
   text_embedding=clip_model.getTextEmbedding(user_text_input)
   #TODO: load everything from database properly done 
-  image_collection = db.get_image_collection() #actually returns image embeddings from database
+  image_collection = db.get_image_collection() #actually returns only image embeddings from database
   result_image_name=get_most_similar_image(text_embedding, image_collection, 1)
   result_image_contents=result_image_name
   return result_image_contents
